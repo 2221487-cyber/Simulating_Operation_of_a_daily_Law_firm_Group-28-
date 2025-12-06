fc63d59e36b6d8eda4f17d3c4f7dd600490149b0
2221487-cyber <2221487@iub.edu.bd>
2025-12-06 16:13:48 +0600

    Delete Simulating_Operation_of_a_daily_Law_firm_Group-28 directory

--- Simulating_Operation_of_a_daily_Law_firm_Group-28/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.simulating_operation_of_a_daily_law_firm_group28 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.simulating_operation_of_a_daily_law_firm_group28 to javafx.fxml;
-    exports com.example.simulating_operation_of_a_daily_law_firm_group28;
-}
